a9c64b54a6924e1e3a17ebe901e5aafebfc4d2f3
Jack-170 <agavejack@hotmail>
2024-09-01 17:02:16 +0200

    update trustproxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = '*'; // Accetta richieste da tutti i proxy
 
     /**
      * The headers that should be used to detect proxies.
